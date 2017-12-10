95c0c121d3b8457b4ae57b85667216bd4b4ded9d
James Gomez <jamesgomez@Virus0XFA003.local>
2017-12-10 14:24:21 -0500

    fixed the package.json and added /client/src/utils/API.js

---
+++ client/src/utils/API.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export default {
+
+  // Gets an artis with the given id
+  getArtist: function(id) {
+    return axios.get("/api/artists/" + id);
+  },
+  // Deletes an artist with the given id
+  deleteArtist: function(id) {
+    return axios.delete("/api/artists/" + id);
+  },
+  // Saves an artist to the database
+  addArtist: function(artistData) {
+    return axios.post("/api/artists", artistData);
+  }
+};
