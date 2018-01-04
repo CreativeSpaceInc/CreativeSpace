d1657217db4b4262acb9a2831ba7572208f861cd
rxi16 <rxi16@users.noreply.github.com>
2018-01-04 13:16:05 -0500

    edited Form directory

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const FormBtn = props =>
   <button  
-  	className="btn btn-block my-4"
+  	className="btn btn-block"
   	type="submit"
   	{...props}>{props.children}
   </button>;
